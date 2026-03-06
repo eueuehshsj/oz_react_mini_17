4e25eac25e73b65273d14dfb0ebb02adae011d7e
이용기 <yg99040177@gmail.com>
2026-03-06 18:43:52 +0900

    배너용 Layout 기반 마련

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -5,17 +5,18 @@ import React from "react";
 const MovieDetail = () => {
   const { poster_path, backdrop_path, title, vote_average, genres, overview } =
     data;
+  const baseUrl = `https://image.tmdb.org/t/p/w500${poster_path}`;
 
   return (
     <div className="detail-page">
       <div className="card">
         <div className="poster">
-          <img src={poster_path} />
+          <img src={baseUrl} alt={title} />
         </div>
       </div>
       <div className="information">
         <div className="title">{title}</div>
-        <div className="vote">평점 : {vote_average}</div>
+        <div className="vote">평점 : {vote_average.toFixed(1)}</div>
         <ul className="genres">
           {genres.map((genre) => (
             <li key={genre.id} className="genre-tag">
