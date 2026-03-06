9d1d54097349f7f566efce38ad94e185c8f856e9
이용기 <yg99040177@gmail.com>
2026-03-06 16:57:44 +0900

    MovieCard 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import MovieDetail from "../pages/MovieDetail.jsx";
-import Home from ".main.jsx";
+import MovieDetail from "./pages/MovieDetail.jsx";
+import Home from "";
 
 function App() {
   return (
     <div className="app">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route pathe="/detail" element={<MovieDetail />} />
+        <Route path="/detail" element={<MovieDetail />} />
       </Routes>
     </div>
   );
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import "./Home.scss";
+import MovieDetail from "./MovieDetail.jsx";
+
+const Home = () => {
+  return (
+    <div className="head">
+      <div className="title">오즈 시네마</div>
+    </div>
+  );
+};
