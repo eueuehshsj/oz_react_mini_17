18913504be1eb7d0d794ba3d9a5b4f5fd5486d66
이용기 <yg99040177@gmail.com>
2026-03-07 18:33:41 +0900

    tailwind css 추가 및 디테일 페이지 재구성

---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex flex-wrap">
+      <img src="../img/Logo.png" alt="Logo" className="w-20 h-20" />
+    </div>
+  );
+};
+
+export default Navbar();
--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,33 +1,32 @@
 import data from "../data/movieDetailData.json";
-import "./MovieDetail.scss";
 import React from "react";
 
-const MovieDetail = () => {
+function MovieDetail() {
   const { poster_path, backdrop_path, title, vote_average, genres, overview } =
     data;
   const baseUrl = `https://image.tmdb.org/t/p/w500${poster_path}`;
 
   return (
-    <div className="detail-page">
-      <div className="card">
-        <div className="poster">
-          <img src={baseUrl} alt={title} />
-        </div>
+    <div className="bg-black flex min-h-screen">
+      <div className="w-100% h-100% relative">
+        <img src={baseUrl} alt={title} />
       </div>
-      <div className="information">
-        <div className="title">{title}</div>
-        <div className="vote">평점 : {vote_average.toFixed(1)}</div>
-        <ul className="genres">
+      <div className="flex flex-col flex-1  p-4 gap-3 text-white">
+        <div className="text-5xl font-bold ">{title}</div>
+        <div className="mt-7 text-xl/normal text-yellow-300 ">
+          평점 : {vote_average.toFixed(1)}/10
+        </div>
+        <ul className="flex  flex-wrap gap-3">
           {genres.map((genre) => (
-            <li key={genre.id} className="genre-tag">
+            <li key={genre.id} className="px-3 py-1 bg-gray-700 rounded-full">
               {genre.name}
             </li>
           ))}
         </ul>
-        <div className="overview">{overview}</div>
+        <div className="text-lg mt-1">{overview}</div>
       </div>
     </div>
   );
-};
+}
 
 export default MovieDetail;
